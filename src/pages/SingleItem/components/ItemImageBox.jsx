8fadc5defe9127046e025ef8f735324558ce4d18
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-22 03:09:48 -0700

    add item image box component and styles

---
+++ src/pages/SingleItem/components/ItemImageBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function ItemImageBox() {
+  return (
+    <div className="item-image-box">
+      <div className="item-image-box__top-div">hi</div>
+      <div className="item-image-box__bottom-div">
+        <div className="item-image-box__bottom-div__image-div">hi</div>
+        <div className="item-image-box__bottom-div__image-div">hi</div>
+        <div className="item-image-box__bottom-div__image-div">hi</div>
+        <div className="item-image-box__bottom-div__image-div">hi</div>
+        <div className="item-image-box__bottom-div__image-div">hi</div>
+      </div>
+    </div>
+  );
+}
