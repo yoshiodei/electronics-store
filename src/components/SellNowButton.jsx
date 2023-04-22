a152fe2aacc2fcbdaf757b5ce72c5c92e8428a54
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-22 11:52:54 -0700

    replace div with link button

--- src/components/SellNowButton.jsx
+++ src/components/SellNowButton.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SellNowButton() {
   // const buttonClicked = () => {
   // }
   return (
-    <div className="sell-now"><h6>Sell Now</h6></div>
+    <Link to="/new-item" className="sell-now">
+      <h6>Sell Now</h6>
+    </Link>
   );
 }
--- src/pages/UserAccount/component/AddPostCard.jsx
+++ src/pages/UserAccount/component/AddPostCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function AddPostCard() {
   return (
-    <div className="add-post-card d-flex">
+    <Link to="/new-item" className="add-post-card d-flex">
       <div className="add-post-card__icon-div d-flex">
         <i className="add-post-card__icon fa-solid fa-plus" />
         <h6 className="add-post-card__text">Add New Post</h6>
       </div>
-    </div>
+    </Link>
   );
 }
