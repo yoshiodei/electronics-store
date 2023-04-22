2727343ee29216d0157f2fc74dc304f7a424c59e
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-22 03:23:07 -0700

    add index script file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import MainRoutes from './routes/MainRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainRoutes />
   </React.StrictMode>
 );
 
