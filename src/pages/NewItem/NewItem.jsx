c6ef72e2721f1fe5d8aeec1c8b0a033c3d9e6ab5
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-22 03:21:17 -0700

    add new item component

---
+++ src/pages/NewItem/NewItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import Footer from '../../sections/Footer';
+import Main from './Main';
+
+export default function NewItem() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
