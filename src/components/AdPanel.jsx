2822c153958e3aad23d92ddb909e1a2e392f4922
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-22 01:34:00 -0700

    add ad panel component

---
+++ src/components/AdPanel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AdPanel() {
+  return (
+    <div className="ad-panel">
+      <h2 className="ad-panel__title">Place Your Ad Here</h2>
+    </div>
+  );
+}
