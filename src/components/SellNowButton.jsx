dcff867d9c98cf117d18bc5a3d95b11bed171b97
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-22 10:59:58 -0700

    update general styles

--- src/components/SellNowButton.jsx
+++ src/components/SellNowButton.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function SellNowButton() {
+  // const buttonClicked = () => {
+  // }
   return (
     <div className="sell-now"><h6>Sell Now</h6></div>
   );
