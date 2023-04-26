5464882c3f07194d7d18181c035521dede81d92d
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-26 00:15:14 -0700

    add link to sell now button

--- src/components/SellNowButton.jsx
+++ src/components/SellNowButton.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SellNowButton() {
-  // const buttonClicked = () => {
-  // }
   return (
-    <Link to="/new-item" className="sell-now">
-      <h6>Sell Now</h6>
-    </Link>
+    <>
+      { false
+        && (
+        <Link to="/new-item" className="sell-now">
+          <h6>Sell Now</h6>
+        </Link>
+        )}
+      <button type="button" className="btn btn-primary sell-now" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Sell Now
+      </button>
+    </>
   );
 }
