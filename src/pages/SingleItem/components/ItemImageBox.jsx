f53df984a8f3eb510000eaf3a2499e843cd84616
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-26 05:40:49 -0700

    modify item image box component and style

--- src/pages/SingleItem/components/ItemImageBox.jsx
+++ src/pages/SingleItem/components/ItemImageBox.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import StartChatButton from './StartChatButton';
 
 export default function ItemImageBox() {
   return (
     <div className="item-image-box">
-      <div className="item-image-box__top-div">hi</div>
+      <div className="item-image-box__top-div">
+        <StartChatButton />
+      </div>
       <div className="item-image-box__bottom-div">
-        <div className="item-image-box__bottom-div__image-div">hi</div>
-        <div className="item-image-box__bottom-div__image-div">hi</div>
-        <div className="item-image-box__bottom-div__image-div">hi</div>
-        <div className="item-image-box__bottom-div__image-div">hi</div>
-        <div className="item-image-box__bottom-div__image-div">hi</div>
+        <div className="item-image-box__bottom-div__image-div" />
+        <div className="item-image-box__bottom-div__image-div" />
+        <div className="item-image-box__bottom-div__image-div" />
+        <div className="item-image-box__bottom-div__image-div" />
+        <div className="item-image-box__bottom-div__image-div" />
       </div>
     </div>
   );
