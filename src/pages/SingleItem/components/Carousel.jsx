2f2e972b92d16325512906b87323d7afeec30a4e
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-28 05:48:42 -0700

    add carousel component and styles

---
+++ src/pages/SingleItem/components/Carousel.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default function Carousel() {
+  return (
+    <div id="carouselExampleIndicators" className="carousel slide carousel__custom">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+        <div className="carousel-item">
+          <img src="..." className="d-block w-100" alt="..." />
+        </div>
+      </div>
+      <button className="carousel-control-prev carousel__custom__button-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true" />
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next carousel__custom__button-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true" />
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+}
