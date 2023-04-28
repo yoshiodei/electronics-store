060649668594645784f9cfcc7d7bbe2901704b8d
Yoshi Odei <yoshiodei@yahoo.com>
2023-04-28 05:45:53 -0700

    add sign up and sign in components and styles

---
+++ src/components/SignUpModal.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function SignUpModal() {
+  return (
+    <div className="modal fade" id="SignUpModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div className="modal-dialog modal-lg">
+        <div className="modal-content">
+          <div className="modal-body">
+            <div className="modal__custom-content d-flex">
+              <div className="modal__custom-content-left">
+                <div>
+                  <h2>Welcome</h2>
+                  <div className="line" />
+                  <p>Create an account today and start selling now on our amazing platform.</p>
+                </div>
+              </div>
+              <div className="modal__custom-content-right">
+                <button type="button" className="btn-close button-close-custom" data-bs-dismiss="modal" aria-label="Close" />
+                <form>
+                  <input placeholder="Enter first name" />
+                  <input placeholder="Enter last name" />
+                  <input placeholder="Enter your email" />
+                  <input placeholder="Enter your mobile number" />
+                  <input placeholder="Enter password" />
+                  <input placeholder="Confirm password" />
+                  <div className="check-box-div">
+                    <input type="checkbox" />
+                    <h6>By checking this box I agree to all the terms and conditions</h6>
+                  </div>
+                  <Link to="/user-account" className="register-button">Register</Link>
+                  <h6 className="switch-to-signin">
+                    Already have an account?
+                    {' '}
+                    <button className="switch-to-signin__button" type="button" data-bs-toggle="modal" data-bs-target="#SignInModal">Sign In</button>
+                  </h6>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/SigninModal.jsx
+++ src/components/SigninModal.jsx
@@ -1,38 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function SigninModal() {
+export default function SignInModal() {
   return (
-    <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div className="modal fade" id="SignInModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog modal-lg">
         <div className="modal-content">
           <div className="modal-body">
             <div className="modal__custom-content d-flex">
               <div className="modal__custom-content-left">
                 <div>
-                  <h2>Welcome</h2>
+                  <h2>Welcome Back</h2>
                   <div className="line" />
-                  <p>Create an account today and start selling now on our amazing platform.</p>
+                  <p>Lorem ipsum ini dolor kalaam sai imei hasman kanal ini sur.</p>
                 </div>
               </div>
               <div className="modal__custom-content-right">
                 <button type="button" className="btn-close button-close-custom" data-bs-dismiss="modal" aria-label="Close" />
                 <form>
-                  <input placeholder="Enter first name" />
-                  <input placeholder="Enter last name" />
-                  <input placeholder="Enter your email" />
-                  <input placeholder="Enter your mobile number" />
+                  <input placeholder="Enter your email or mobile number" />
                   <input placeholder="Enter password" />
-                  <input placeholder="Confirm password" />
-                  <div className="check-box-div">
-                    <input type="checkbox" />
-                    <h6>By checking this box I agree to all the terms and conditions</h6>
-                  </div>
-                  <Link to="/user-account" className="register-button">Register</Link>
+                  <Link to="/user-account" className="register-button">Sign In</Link>
                   <h6 className="switch-to-signin">
-                    Already have an account?
+                    Don&apos;t have an account?
                     {' '}
-                    <span>Sign In</span>
+                    <button className="switch-to-signin__button" type="button" data-bs-toggle="modal" data-bs-target="#SignUpModal">Register</button>
                   </h6>
                 </form>
               </div>
