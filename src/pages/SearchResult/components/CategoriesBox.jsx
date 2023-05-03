6aac0378f149d49fd564f9172f72a40a477094c8
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-03 01:32:30 -0700

    add search categories box component and styles

---
+++ src/pages/SearchResult/components/CategoriesBox.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export default function CategoriesBox() {
+  return (
+    <div className="search__categories-box">
+      <ul>
+        <li>
+          <button type="button">Phones</button>
+        </li>
+        <li>
+          <button type="button">Televisions</button>
+        </li>
+        <li>
+          <button type="button">Desktops</button>
+        </li>
+        <li>
+          <button type="button">Laptop</button>
+        </li>
+        <li>
+          <button type="button">Gaming Consoles</button>
+        </li>
+        <li>
+          <button type="button">Headphones and Speakers</button>
+        </li>
+        <li>
+          <button type="button">Accessories</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
