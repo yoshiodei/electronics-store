41d8cd852e9c6109a1cd262c173799f6504fa8d9
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-08 15:50:13 -0700

    add store and provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 /* eslint-disable */
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MainRoutes from './routes/MainRoutes';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainRoutes />
+    <Provider store={store}>
+      <MainRoutes />
+    </Provider>
   </React.StrictMode>
 );
 
