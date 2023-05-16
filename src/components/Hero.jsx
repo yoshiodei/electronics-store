b56b4c02ea8052395785431c1365b7b3fd87b665
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-16 08:26:16 -0700

    minor changes in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import guyPointingDown from '../assets/images/guyPointingDown.png';
 import womanHoldingLaptop from '../assets/images/womanHoldingLaptop.png';
 import SellNowButton from './SellNowButton';
 
 export default function Hero() {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/search/${search}`);
+  };
+
   return (
     <div className="hero">
       <div className="hero__main-div">
@@ -14,9 +23,9 @@ export default function Hero() {
       </div>
       <div className="hero__search-div d-flex">
         <div className="hero__form">
-          <form>
-            <input className="hero__search-div__input" placeholder="What are you looking for?" />
-            <button type="button" className="hero__search-div__button">Search</button>
+          <form className="hero__form-element" onSubmit={handleSubmit}>
+            <input className="hero__search-div__input" value={search} placeholder="What are you looking for?" onChange={(e) => setSearch(e.target.value)} />
+            <button type="submit" className="hero__search-div__button">Search</button>
           </form>
         </div>
         <img src={guyPointingDown} className="hero__search-div__image" alt="guy pointing down" />
