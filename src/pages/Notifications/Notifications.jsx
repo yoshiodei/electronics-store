400c64a6b1f21290a3bba879cbdeac42ee9157cd
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-16 11:47:28 -0700

    modify notification

--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import Navbar from '../../components/Navbar';
-import Main from './Main';
 import Footer from '../../sections/Footer';
+import { selectAuthState } from '../../redux/slice/authSlice';
+import Main from './Main';
 
 export default function Notifications() {
+  const { userId } = useSelector(selectAuthState);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!userId) {
+      navigate('/');
+      toast.error('Access Denied: Unauthorized Page', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+      });
+    }
+  }, []);
+
   return (
     <>
       <Navbar />
