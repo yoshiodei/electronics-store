6abed64717cb81e74b46dd9a1aeb4407e80a066c
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-19 09:09:57 -0700

    minor changes in vendor details component

--- src/pages/SingleItem/components/VendorDetails.jsx
+++ src/pages/SingleItem/components/VendorDetails.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { doc, getDoc } from 'firebase/firestore';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { db } from '../../../config/firebaseConfig';
 import VendorDetailLoading from './VendorDetailLoading';
+import placeholderImage from '../../../assets/images/profile.jpg';
 
 export default function VendorDetails() {
   const [product, setProduct] = useState({});
@@ -37,12 +38,15 @@ export default function VendorDetails() {
     <div className="vendor-details">
       <div className="vendor-details__info-div d-flex">
         <div className="vendor-details__profile-image-div">
-          <img alt="user" />
+          {!product.image && <img src={placeholderImage} alt="user" />}
+          {product.image && <img src={product.vendor.image} alt="user" />}
           <div className="vendor-details__online-indicator" />
         </div>
         <div className="vendor-details__profile-info-div">
           <h6 className="vendor-details__profile-title">Vendor</h6>
-          <h5 className="vendor-details__profile-name">John Doe</h5>
+          <Link to={`/user-account/${product.vendor.userId}`}>
+            <h5 className="vendor-details__profile-name">{product.vendor.displayName}</h5>
+          </Link>
         </div>
       </div>
     </div>
