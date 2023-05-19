ac3e9d68960921ccf85ba2a0dfda982176b4fe38
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-19 09:11:40 -0700

    add remove all button

---
+++ src/pages/WishList/components/RemoveAllButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RemoveAllButton() {
+  return (
+    <div className="wish-list__remove-all-div">
+      <button type="button" className="wish-list__remove-all-button">Remove All Items</button>
+    </div>
+  );
+}
