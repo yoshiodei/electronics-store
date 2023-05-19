966e9da67f4c9ae00111599f8d0f408c1af26c2d
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-19 09:08:20 -0700

    modify wish list component and styles

---
+++ src/pages/WishList/components/WishListItem.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function WishListItem({ item, removeItem }) {
+  const {
+    name, price, image, location, condition, id,
+  } = item;
+
+  const handleCloseItem = () => {
+    removeItem(id);
+  };
+
+  return (
+    <div className="wish-list__item" key={id}>
+      <Link to={`/single-item/${id}`}>
+        <div className="wish-list__image-div">
+          <img src={image} alt="..." />
+        </div>
+        <div className="wish-list__info-div">
+          <h6>{name}</h6>
+          <h6>{`$ ${price}.00`}</h6>
+          <h6>{location}</h6>
+          <h6>{condition}</h6>
+        </div>
+      </Link>
+      <div className="wish-list__button-div">
+        <button type="button" onClick={() => handleCloseItem()}>X</button>
+      </div>
+    </div>
+  );
+}
