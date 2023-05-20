63292a1aaa906cbec33d5133dcde5c0349953382
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-19 23:40:42 -0700

    update add to wish list

--- src/redux/slice/wishListSlice.js
+++ src/redux/slice/wishListSlice.js
@@ -1,31 +1,8 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { doc, updateDoc } from 'firebase/firestore';
-import { useSelector } from 'react-redux';
-import { selectAuthState } from './authSlice';
-import { db } from '../../config/firebaseConfig';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   wishList: [],
 };
-
-const { docId } = useSelector(selectAuthState);
-
-const wishListRef = doc(db, 'vendor', docId);
-
-export const updateWishlist = createAsyncThunk(
-  'vendor/updateWishlist',
-  async ({ vendorId, wishlist }) => {
-    try {
-      await updateDoc(wishListRef, {
-        wishlist: true,
-      });
-
-      return { vendorId, wishlist };
-    } catch (error) {
-      throw new Error(`Error updating wishlist: ${error}`);
-    }
-  },
-);
 
 const wishListSlice = createSlice({
   name: 'wishList',
