62e519e1394b41c02e0e40f3288668d95fa86a17
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-27 04:46:40 -0700

    update firebase config files

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getDatabase } from 'firebase/database';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyAkUWg1AWJdR1OK48zUbnXrpUI2tfJTVX4',
@@ -10,9 +11,11 @@ export const firebaseConfig = {
   storageBucket: 'electronics-shop-82ad4.appspot.com',
   messagingSenderId: '97497925549',
   appId: '1:97497925549:web:b61eb9e6a551b35574c324',
+  databaseURL: 'https://electronics-shop-82ad4-default-rtdb.firebaseio.com/',
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const database = getDatabase(app);
 export const storage = getStorage(app);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slice/authSlice';
 import wishListReducer from './slice/wishListSlice';
+import chatReducer from './slice/chatSlice';
+import productsReducer from './slice/productsSlice';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   wishList: wishListReducer,
+  chat: chatReducer,
+  products: productsReducer,
 });
 
 const store = configureStore({
