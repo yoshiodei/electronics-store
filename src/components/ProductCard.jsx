c6c8bcd74c54b1feb87251d1ffd742badd0d8c07
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-27 04:51:48 -0700

    update unused code

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,44 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-  doc, updateDoc, arrayUnion,
-} from 'firebase/firestore';
 import { useDispatch, useSelector } from 'react-redux';
-import { db } from '../config/firebaseConfig';
+// import { db } from '../config/firebaseConfig';
 import { selectAuthState } from '../redux/slice/authSlice';
-import { ADD_TO_WISHLIST, selectWishListState } from '../redux/slice/wishListSlice';
+import { addToWhishList } from '../redux/slice/wishListSlice';
 
 export default function ProductCard({ product }) {
   const {
     id, price, name, location, condition, isPromoted, image,
   } = product;
 
-  const { docId, isLoggedIn } = useSelector(selectAuthState);
-  const { wishList } = useSelector(selectWishListState);
+  const { isLoggedIn, docId } = useSelector(selectAuthState);
+  // const { wishList } = useSelector(selectWishListState);
   const dispatch = useDispatch();
 
   const handleAddToWishList = async (wishListProduct) => {
     if (!isLoggedIn) {
-      try {
-        const vendorRef = doc(db, 'vendors', docId);
-        await updateDoc(vendorRef, {
-          wishlist: arrayUnion(wishListProduct),
-        });
-      } catch (err) {
-        console.log(err.message);
-      }
+      console.log('Log in to add items to wish list');
     } else {
-      const isFound = wishList.some((item, index) => {
-        console.log(`test ${index} => ${item.id} vs ${wishListProduct.id}`);
-        return item.id === wishListProduct.id;
-      });
-      console.log(`is found is ${isFound}`);
-      if (!isFound) {
-        dispatch(ADD_TO_WISHLIST(wishListProduct));
-        console.log(wishList);
-      } else {
-        console.log('item is already in list');
-      }
+      dispatch(addToWhishList({ ...wishListProduct, docId }));
     }
   };
 
