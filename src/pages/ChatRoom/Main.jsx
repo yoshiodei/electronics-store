b7d88050628481ee40c75cf8ece917b4f67984c5
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-27 04:53:06 -0700

    minor changes to main component

--- src/pages/ChatRoom/Main.jsx
+++ src/pages/ChatRoom/Main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import AdPanel from '../../components/AdPanel';
 import ChatWall from './components/ChatWall';
-import VendorProfile from './components/VendorProfile';
+// import VendorProfile from './components/VendorProfile';
+import ChatList from './components/ChatList';
 
 export default function Main() {
   return (
     <div className="main-section-div">
       <main className="main-section d-flex justify-content-between">
         <div className="main-section__left-div">
-          <VendorProfile />
+          <ChatList />
+          {/* <VendorProfile /> */}
           <AdPanel />
         </div>
         <div className="main-section__right-div">
