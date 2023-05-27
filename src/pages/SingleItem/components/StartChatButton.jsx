1cbc6ff7aba706f5c6e30c49cc7e4ddbe3758a8a
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-27 04:52:29 -0700

    add start chat button component

--- src/pages/SingleItem/components/StartChatButton.jsx
+++ src/pages/SingleItem/components/StartChatButton.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { doc, updateDoc, arrayUnion } from 'firebase/firestore';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { SET_RECIPIENT_CHAT_DETAILS } from '../../../redux/slice/chatSlice';
+import { selectAuthState } from '../../../redux/slice/authSlice';
+import { db } from '../../../config/firebaseConfig';
 
-export default function StartChatButton() {
+export default function StartChatButton({ recipientData }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { docId } = useSelector(selectAuthState);
+
+  const handleButtonClick = () => {
+    dispatch(SET_RECIPIENT_CHAT_DETAILS(recipientData));
+
+    const vendorRef = doc(db, 'vendors', docId);
+
+    updateDoc(vendorRef, {
+      chatList: arrayUnion({
+        recipientName: recipientData.vendor.displayName,
+        recipientId: recipientData.vendorId,
+        recipientDocId: recipientData.vendorDocId,
+        recipientImage: recipientData.vendor.image,
+      }),
+    })
+      .then(() => {
+        navigate('/chat-room');
+      })
+      .catch(
+        (err) => {
+          console.log('No such document!', err.message);
+        },
+      );
+  };
+
   return (
-    <Link to="/chat-room" className="start-chat-button" title="message vendor">
+    <button type="button" className="start-chat-button" title="message vendor" onClick={handleButtonClick}>
       <i className="fa-regular fa-message" />
-    </Link>
+    </button>
   );
 }
