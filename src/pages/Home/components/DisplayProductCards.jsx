927337bdc6255637f1b54a5d8fdc66979d7db372
Yoshi Odei <yoshiodei@yahoo.com>
2023-05-27 04:41:33 -0700

    modify home display card

--- src/pages/Home/components/DisplayProductCards.jsx
+++ src/pages/Home/components/DisplayProductCards.jsx
@@ -2,12 +2,22 @@ import React, { useEffect, useState } from 'react';
 import {
   collection, query, where, getDocs,
 } from '@firebase/firestore';
+import {
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 import { db } from '../../../config/firebaseConfig';
 import ProductCard from '../../../components/ProductCard';
 import DisplayProductLoader from './DisplayProductLoader';
+import {
+  SET_PRODUCTS_LIST,
+  selectProductsState,
+} from '../../../redux/slice/productsSlice';
 
 export default function DisplayProductCards() {
   const [products, setProducts] = useState([]);
+  const { productsList } = useSelector(selectProductsState);
+  const dispatch = useDispatch();
 
   const fetchData = async () => {
     try {
@@ -25,14 +35,22 @@ export default function DisplayProductCards() {
         const data = doc.data();
         allProducts.push({ ...data, id: doc.id });
       });
+
       setProducts(allProducts);
+      dispatch(SET_PRODUCTS_LIST(products));
     } catch (err) {
       console.log(err.message);
     }
   };
 
   useEffect(() => {
-    fetchData();
+    if (productsList.length === 0) {
+      console.log('is loaded');
+      fetchData();
+    } else {
+      console.log('not loaded');
+      setProducts(productsList);
+    }
   }, []);
 
   if (!products.length > 0) {
