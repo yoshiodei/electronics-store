ba94652f7945b25ddc57dfb19f1ff979ecd6464c
Yoshi Odei <yoshiodei@yahoo.com>
2023-06-04 04:01:17 -0700

    modify product slice

--- src/redux/slice/productsSlice.js
+++ src/redux/slice/productsSlice.js
@@ -1,18 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAction, createSlice } from '@reduxjs/toolkit';
+
+export const setFilter = createAction('setFilter');
+export const resetFilter = createAction('resetFilter');
+export const emptyProductsList = createAction('emptyProductsList');
+export const fillProductsList = createAction('fillProductsList');
+
+const initialFilter = {
+  maxPrice: 10000,
+  minPrice: 0,
+  location: 'all',
+  category: 'all',
+  condition: 'all',
+  updateTime: 0,
+};
 
 const initialState = {
   isLoading: false,
   productsList: [],
+  filterObject: initialFilter,
   error: null,
 };
 
 const productsSlice = createSlice({
   name: 'products',
   initialState,
-  reducers: {
-    SET_PRODUCTS_LIST(state, action) {
-      state.productsList = action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(setFilter, (state, action) => {
+        state.filterObject = action.payload;
+      })
+      .addCase(resetFilter, (state) => {
+        state.filterObject = initialFilter;
+      })
+      .addCase(emptyProductsList, (state) => {
+        state.productsList = [];
+      })
+      .addCase(fillProductsList, (state, action) => {
+        state.productsList = action.payload;
+      });
   },
 });
 
