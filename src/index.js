3e87d7daf409f1c35bb41d66611dcfe4b778054d
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-09 02:40:27 +0000

    modify root file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 /* eslint-disable */
 import React from 'react';
+import '@stripe/stripe-js';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MainRoutes from './routes/MainRoutes';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'react-toastify/dist/ReactToastify.css';
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en.json';
+
+TimeAgo.addDefaultLocale(en);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
