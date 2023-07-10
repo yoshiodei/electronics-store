95c147c881ed3bf21cf85c899055ff6d52dd51dc
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-10 22:25:55 +0000

    remove unused import

--- src/pages/SingleItem/components/VendorDetails.jsx
+++ src/pages/SingleItem/components/VendorDetails.jsx
@@ -3,7 +3,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import { useParams, Link } from 'react-router-dom';
 import { db } from '../../../config/firebaseConfig';
 import VendorDetailLoading from './VendorDetailLoading';
-import placeholderImage from '../../../assets/images/profile.jpg';
+import profile from '../../../assets/images/profile.jpg';
 
 export default function VendorDetails() {
   const [product, setProduct] = useState({});
@@ -38,9 +38,7 @@ export default function VendorDetails() {
     <div className="vendor-details">
       <div className="vendor-details__info-div d-flex">
         <div className="vendor-details__profile-image-div">
-          {!product.image && <img src={placeholderImage} alt="user" />}
-          {product.image && <img src={product.vendor.image} alt="user" />}
-          <div className="vendor-details__online-indicator" />
+          <img src={product?.vendor?.image || profile} alt="user" />
         </div>
         <div className="vendor-details__profile-info-div">
           <h6 className="vendor-details__profile-title">Vendor</h6>
