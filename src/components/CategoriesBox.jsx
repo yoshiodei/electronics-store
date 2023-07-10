6a149f5f699cea7f1713d1f43924e3a89f876356
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-10 21:38:10 +0000

    add dispatch function to categories box

--- src/components/CategoriesBox.jsx
+++ src/components/CategoriesBox.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setFilter } from '../redux/slice/productsSlice';
 
 export default function CategoriesBox() {
+  const dispatch = useDispatch();
+
+  const initialFilter = {
+    maxPrice: 10000,
+    minPrice: 0,
+    location: 'all',
+    category: 'all',
+    condition: 'all',
+  };
+
+  const handleClearFilter = () => {
+    dispatch(setFilter({ ...initialFilter, updateTime: Date.now() }));
+  };
+
   return (
     <div className="categories-box">
       <div className="categories-box__title-div d-flex align-items-center">
@@ -14,7 +30,7 @@ export default function CategoriesBox() {
       </div>
       <ul className="categories-box__list d-flex flex-column justify-content-between">
         <li className="categories-box__list-item">
-          <Link to="/category/phones">
+          <Link to="/category/phones" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-mobile me-2" />
               <p>Phones</p>
@@ -23,7 +39,7 @@ export default function CategoriesBox() {
           </Link>
         </li>
         <li className="categories-box__list-item categories-box__list-item">
-          <Link to="/category/televisions">
+          <Link to="/category/televisions" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-tv me-2" />
               <p>Televisions</p>
@@ -32,7 +48,7 @@ export default function CategoriesBox() {
           </Link>
         </li>
         <li className="categories-box__list-item">
-          <Link to="/category/desktops">
+          <Link to="/category/desktops" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-desktop me-2" />
               <p>Desktops</p>
@@ -41,7 +57,7 @@ export default function CategoriesBox() {
           </Link>
         </li>
         <li className="categories-box__list-item">
-          <Link to="/category/laptops">
+          <Link to="/category/laptops" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-laptop me-2" />
               <p>Laptops</p>
@@ -50,7 +66,7 @@ export default function CategoriesBox() {
           </Link>
         </li>
         <li className="categories-box__list-item">
-          <Link to="/category/game-consoles">
+          <Link to="/category/game consoles" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-gamepad me-2" />
               <p>Game Consoles</p>
@@ -59,7 +75,7 @@ export default function CategoriesBox() {
           </Link>
         </li>
         <li className="categories-box__list-item">
-          <Link to="/category/headphones-and-speakers">
+          <Link to="/category/headphones and speakers" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-headphones me-2" />
               <p>Headphones & Speakers</p>
@@ -68,7 +84,7 @@ export default function CategoriesBox() {
           </Link>
         </li>
         <li className="categories-box__list-item">
-          <Link to="/category/accessories">
+          <Link to="/category/accessories" onClick={handleClearFilter}>
             <div className="d-flex align-items-center">
               <i className="fa-solid fa-puzzle-piece me-2" />
               <p>Accessories</p>
--- src/pages/SearchResult/components/CategoriesBox.jsx
+++ src/pages/SearchResult/components/CategoriesBox.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { setFilter } from '../../../redux/slice/productsSlice';
 
 export default function CategoriesBox() {
+  const initialFilter = {
+    maxPrice: 10000,
+    minPrice: 0,
+    location: 'all',
+    category: 'all',
+    condition: 'all',
+  };
+
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleClearFilter = (link) => {
+    dispatch(setFilter({ ...initialFilter, updateTime: Date.now() }));
+    navigate(link);
+  };
 
   return (
     <div className="search__categories-box">
       <ul>
         <li>
-          <button type="button" onClick={() => navigate('/category/phones')}>Phones</button>
+          <button type="button" onClick={() => handleClearFilter('/category/phones')}>Phones</button>
         </li>
         <li>
-          <button type="button" onClick={() => navigate('/category/televisions')}>Televisions</button>
+          <button type="button" onClick={() => handleClearFilter('/category/televisions')}>Televisions</button>
         </li>
         <li>
-          <button type="button" onClick={() => navigate('/category/desktops')}>Desktops</button>
+          <button type="button" onClick={() => handleClearFilter('/category/desktops')}>Desktops</button>
         </li>
         <li>
-          <button type="button" onClick={() => navigate('/category/laptops')}>Laptops</button>
+          <button type="button" onClick={() => handleClearFilter('/category/laptops')}>Laptops</button>
         </li>
         <li>
-          <button type="button" onClick={() => navigate('/category/headphones-and-speakers')}>Headphones and Speakers</button>
+          <button type="button" onClick={() => handleClearFilter('/category/headphones and speakers')}>Headphones and Speakers</button>
         </li>
         <li>
-          <button type="button" onClick={() => navigate('/category/gaming-consoles')}>Gaming Consoles</button>
+          <button type="button" onClick={() => handleClearFilter('/category/game consoles')}>Game Consoles</button>
         </li>
         <li>
-          <button type="button" onClick={() => navigate('/category/accessories')}>Accessories</button>
+          <button type="button" onClick={() => handleClearFilter('/category/accessories')}>Accessories</button>
         </li>
       </ul>
     </div>
