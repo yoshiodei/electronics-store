3ed77309f094c64500c8699f2c79fd76090dc2d3
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-10 21:39:09 +0000

    add pagination component and styles

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Pagination() {
+export default function Pagination({ products }) {
+  const [pagesArray, setPagesArray] = useState([]);
+  // const handleChangePage = (page) => {
+  //   setPage(page);
+  // };
+
+  // const handlePrev = () => {
+  //   setPage(--page);
+  //   console.log(page);
+  // };
+
+  // const handleNext = () => {
+  //   setPage(++page);
+  // };
+
+  // useEffect(() => {
+  const numberOfPages = Math.ceil((products.length / 20));
+  for (let i = 1; i <= numberOfPages; i += 1) {
+    setPagesArray([...pagesArray, i.toString()]);
+  }
+  // }, []);
+
   return (
     <div className="pagination-div d-flex">
       <div className="pagination d-flex">
         <h6>Prev</h6>
         <div className="pagination__buttons-div d-flex">
-          <div className="pagination__button">1</div>
-          <div className="pagination__button">2</div>
-          <div className="pagination__button">3</div>
-          <div>...</div>
-          <div className="pagination__button">10</div>
+          {pagesArray.map((page) => (
+            <div className="pagination__button">{page}</div>
+          ))}
         </div>
         <h6>Next</h6>
       </div>
