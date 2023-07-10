0cbb56ab8790e512be84b2de7c0fbe59a21cffb4
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-10 22:19:08 +0000

    add edit profile page

---
+++ src/pages/EditProfile/EditProfile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import Main from './Main';
+import Footer from '../../sections/Footer';
+
+export default function EditProfile() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
