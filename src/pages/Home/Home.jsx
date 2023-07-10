ea6287fbe6ae05b06b7c49b02c4a9fa1b7f3d7de
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-10 22:25:34 +0000

    make minor changes to home component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import Footer from '../../sections/Footer';
+import Main from '../../sections/HomeMain';
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
