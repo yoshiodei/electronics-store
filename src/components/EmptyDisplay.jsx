36590e16148a918cbf01d2dbb846ccf1c30df8dc
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-13 01:35:26 +0000

    add component for loading states

---
+++ src/components/EmptyDisplay.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function EmptyDisplay() {
+  return (
+    <div className="tabs-custom__empty-div">
+      <i className="fa-solid fa-face-sad-tear" />
+      <h2>There are no items to display</h2>
+    </div>
+  );
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Loader() {
+  return (
+    <div className="loader__container">
+      <div className="loader__div">
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  );
+}
