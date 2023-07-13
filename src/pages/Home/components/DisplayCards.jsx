1a95aaf725650caae43606085e011bfa3dbf2989
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-13 01:43:50 +0000

    remove unused imports from component

--- src/pages/Home/components/DisplayCards.jsx
+++ src/pages/Home/components/DisplayCards.jsx
@@ -1,45 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   collection, getDocs, query, where,
 } from '@firebase/firestore';
-import { selectProductsState } from '../../../redux/slice/productsSlice';
+import { fillProductsList, selectProductsState } from '../../../redux/slice/productsSlice';
 import { db } from '../../../config/firebaseConfig';
 import ProductCard from '../../../components/ProductCard';
-// import useCheckCloseItems from '../../../components/Hooks/useCheckCloseItems';
+import Loader from '../../../components/Loader';
+import EmptyDisplay from '../../../components/EmptyDisplay';
 
 export default function DisplayCards() {
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(32);
+  const dispatch = useDispatch();
 
-  const { filterObject, userCoordinates } = useSelector(selectProductsState);
+  const { filterObject, userCoordinates, productsList } = useSelector(selectProductsState);
   const {
     updateTime: time, maxPrice, minPrice, location, category, condition,
   } = filterObject;
 
   useEffect(() => {
     const fetchData = async () => {
-      const q = query(collection(db, 'products'), where('isPromoted', '==', true));
-      const querySnapshot = await getDocs(q);
-      const allProducts = [];
-      querySnapshot.forEach((doc) => {
-        const queryData = doc.data();
-        allProducts.push({ ...queryData, id: doc.id });
-      });
+      const loading = true;
+      setIsLoading(loading);
 
-      const q2 = query(collection(db, 'products'), where('isPromoted', '==', false));
-      const querySnapshot2 = await getDocs(q2);
-      querySnapshot2.forEach((doc) => {
-        const queryData = doc.data();
-        allProducts.push({ ...queryData, id: doc.id });
-      });
+      if (productsList.length > 1) {
+        setData(productsList);
+        setFilteredData(productsList);
+      } else {
+        try {
+          const q = query(collection(db, 'products'), where('isPromoted', '==', true));
+          const querySnapshot = await getDocs(q);
+          const allProducts = [];
+          querySnapshot.forEach((doc) => {
+            const queryData = doc.data();
+            allProducts.push({ ...queryData, id: doc.id });
+          });
 
-      setData(allProducts);
-      setFilteredData(allProducts);
+          const q2 = query(collection(db, 'products'), where('isPromoted', '==', false));
+          const querySnapshot2 = await getDocs(q2);
+          querySnapshot2.forEach((doc) => {
+            const queryData = doc.data();
+            allProducts.push({ ...queryData, id: doc.id });
+          });
+
+          setData(allProducts);
+          dispatch(fillProductsList(allProducts));
+          setFilteredData(allProducts);
+        } catch (error) {
+          console.log(error.message);
+          setIsLoading(false);
+        }
+      }
     };
-
     fetchData();
   }, []);
 
@@ -73,30 +89,47 @@ export default function DisplayCards() {
   };
 
   useEffect(() => {
-    const filtered = data.filter(
-      (item) => (
-        item.price >= minPrice
+    const filterData = async () => {
+      const loading = true;
+      setIsLoading(loading);
+
+      try {
+        const filtered = data.filter(
+          (item) => (
+            item.price >= minPrice
         && item.price <= maxPrice
         && (item.condition === condition || condition === 'all')
         && (itemWithin5miles(item) || location === 'all')
         && (item.category === category || category === 'all')
-      ),
-    );
+          ),
+        );
+        setFilteredData(filtered);
+        setIsLoading(!loading);
+        setCurrentPage(1);
+      } catch (error) {
+        console.log(error.message);
+        setIsLoading(false);
+      }
+    };
 
-    setFilteredData(filtered);
-    console.log('filteredData -->', filteredData);
-    setCurrentPage(1);
+    filterData();
   }, [data, time]);
 
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
 
-  console.log('currentItems --->', currentItems);
-
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
+
+  if (isLoading === true || data.length === 0) {
+    return (<Loader />);
+  }
+
+  if (filteredData.length === 0) {
+    return (<EmptyDisplay />);
+  }
 
   return (
     <>
