9aba0d51e2291da64fb1df82706567e62a47fd50
Yoshi Odei <yoshiodei@yahoo.com>
2023-07-13 01:35:55 +0000

    add main component and styles

--- src/pages/Category/Main.jsx
+++ src/pages/Category/Main.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import AdPanel from '../../components/AdPanel';
-import FilterCard from '../../components/FilterCard';
 import SectionHeader from '../../components/SectionHeader';
 import CategoriesBox from '../SearchResult/components/CategoriesBox';
 import SearchBar from '../SearchResult/components/SearchBar';
-import DispalyCategoryProducts from './components/DispalyCategoryProducts';
+import DisplayCategoryProducts from './components/DisplayCategoryProducts';
 import CategoryInfoBox from './components/CategoryInfoBox';
+import CategoryFilterCard from './components/CategoryFilterCard';
 
 export default function Main() {
+  const { category } = useParams();
+  console.log(category);
+
   return (
     <div className="main-section-div">
       <main className="main-section d-flex justify-content-between">
         <div className="main-section__left-div">
           <SectionHeader>Filter</SectionHeader>
-          <FilterCard />
+          <CategoryFilterCard category={category} />
           <AdPanel />
         </div>
         <div className="main-section__right-div">
           <CategoryInfoBox />
           <CategoriesBox />
           <SearchBar />
-          <DispalyCategoryProducts />
+          <DisplayCategoryProducts />
         </div>
       </main>
     </div>
