a8c698b3d657833a306faedc4655853fdcea881c
Yoshi Odei <yoshiodei@yahoo.com>
2023-08-26 07:49:51 +0000

    add google ad sense again

--- src/components/AdPanel.jsx
+++ src/components/AdPanel.jsx
@@ -10,16 +10,16 @@ export default function AdPanel() {
   }, []);
 
   return (
-    <div className="ad-panel">
-      <h2 className="ad-panel__title">Place Your Ad Here</h2>
-    </div>
-    // <ins
-    //   className="ad-panel"
-    //   data-ad-client="ca-pub-1115234717744723"
-    //   data-ad-slot="3903146717"
-    //   data-ad-format="auto"
-    //   data-full-width-responsive="true"
-    // />
+    // <div className="ad-panel">
+    //   <h2 className="ad-panel__title">Place Your Ad Here</h2>
+    // </div>
+    <ins
+      className="ad-panel"
+      data-ad-client="ca-pub-1115234717744723"
+      data-ad-slot="3903146717"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
 
   );
 }
